089b0fce7f8b8415ee71b2dba551da9362e4873a
hungle910 <70809735+hungle910@users.noreply.github.com>
2021-07-04 22:50:39 +0700

    Update app.js line 14

--- count-app/src/App.js
+++ count-app/src/App.js
@@ -3,27 +3,7 @@ import './App.css';
 import { Component } from 'react';
 
 import Counters from "./components/counters"
-// function App() {
-//   return (
-//     // <div className="App">
-//     //   <header className="App-header">
-//     //     <img src={logo} className="App-logo" alt="logo" />
-//     //     <p>
-//     //       Edit <code>src/App.js</code> and save to reload.
-//     //     </p>
-//     //     <a
-//     //       className="App-link"
-//     //       href="https://reactjs.org"
-//     //       target="_blank"
-//     //       rel="noopener noreferrer"
-//     //     >
-//     //       Learn React
-//     //     </a>
-//     //   </header>
-//     // </div>
-//     <Counters />
-//   );
-// }
+
 
 class App extends Component{
   render(){
@@ -31,6 +11,7 @@ class App extends Component{
 
       <>
         <h1>This is return of App.js</h1>
+        <h1>Hello</h1>
         <Counters />
       </>
     )
